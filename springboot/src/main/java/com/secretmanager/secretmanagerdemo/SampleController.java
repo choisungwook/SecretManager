e86d404794c766993f274943a19d3f1f8e2058b7
choisungwook <kgg1959@naver.com>
2023-11-19 15:19:25 +0900

    KAN-30 Add intergreate secret manager and springboot

--- springboot/src/main/java/com/secretmanager/secretmanagerdemo/SampleController.java
+++ springboot/src/main/java/com/secretmanager/secretmanagerdemo/SampleController.java
@@ -1,13 +1,22 @@
 package com.secretmanager.secretmanagerdemo;
 
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableAutoConfiguration
 public class SampleController {
-    @GetMapping("/helloworld")
-    String home() {
-        return "Hello World!";
+    @Value("${PASSWORD}")
+    String password;
+
+    @Value("${USERNAME}")
+    String usernmae;
+
+    @GetMapping("/info")
+    String info() {
+        return String.format("%s: %s", usernmae, password);
     }
 }
